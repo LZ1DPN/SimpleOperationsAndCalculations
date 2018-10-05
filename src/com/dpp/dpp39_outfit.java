17677c57da64acbce76e7d54e8f15281ed776bc2
user <lz1dpn@yahoo.com>
2018-10-05 16:37:59 +0300

    05.10.2018

--- src/com/dpp/dpp39_outfit.java
+++ src/com/dpp/dpp39_outfit.java
@@ -5,24 +5,57 @@ import java.util.Scanner;
 public class dpp39_outfit {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String year = scanner.nextLine();
-        Double p = Double.parseDouble(scanner.nextLine());
-        Double h = Double.parseDouble(scanner.nextLine());
-        Double weekend = 48.00;
-        Double weekSofia = (weekend - h);
-        Double games = (((weekSofia * 3)/4) + h + ((p * 2)/3));
+        Double degree = Double.parseDouble(scanner.nextLine());
+        String dayTime = scanner.nextLine();
+        String outfit = "";
+        String shoes = "";
 
-        switch (year) {
-            case "leap":
-                games = Math.floor(games + (games * 0.15));
+        switch (dayTime) {
+            case "Morning":
+                if ((10 <= degree) && (degree <= 18)){
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+                }
+                if ((18 < degree) && (degree <= 24)){
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                if (degree >= 25){
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
                 break;
-            case "normal":
-                games = Math.floor(games);
+            case "Afternoon":
+                if ((10 <= degree) && (degree <= 18)){
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                if ((18 < degree) && (degree <= 24)){
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                if (degree >= 25){
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+                break;
+            case "Evening":
+                if ((10 <= degree) && (degree <= 18)){
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                if ((18 < degree) && (degree <= 24)){
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                if (degree >= 25){
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
                 break;
             default:
-                games = Math.floor(games);
                 break;
         }
-        System.out.printf("%.0f", games);
+        System.out.printf("It's %.0f degrees, get your %s and %s.", degree, outfit, shoes);
     }
 }
