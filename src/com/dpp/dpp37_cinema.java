1b3c26b314d278f6730b4144e117f51121e0b346
user <lz1dpn@yahoo.com>
2018-10-05 16:11:50 +0300

    05.10.2018

--- src/com/dpp/dpp37_cinema.java
+++ src/com/dpp/dpp37_cinema.java
@@ -1,5 +1,30 @@
 package com.dpp;
 
-public class dpp37_cinema {
+import java.util.Scanner;
 
+public class dpp37_cinema {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String typeMovie = scanner.nextLine();
+        Double r = Double.parseDouble(scanner.nextLine());
+        Double c = Double.parseDouble(scanner.nextLine());
+        Double price = 0.00;
+
+        switch (typeMovie) {
+            case "Premiere":
+                price = 12.00;
+                break;
+            case "Normal":
+                price = 7.50;
+                break;
+            case "Discount":
+                price = 5.00;
+                break;
+            default:
+                price = 7.50;
+                break;
+        }
+        price = price * r * c;
+        System.out.printf("%.2f leva", price);
+    }
 }
---
+++ src/com/dpp/dpp38_voleyball.java
@@ -0,0 +1,28 @@
+package com.dpp;
+
+import java.util.Scanner;
+
+public class dpp38_voleyball {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String year = scanner.nextLine();
+        Double p = Double.parseDouble(scanner.nextLine());
+        Double h = Double.parseDouble(scanner.nextLine());
+        Double weekend = 48.00;
+        Double weekSofia = (weekend - h);
+        Double games = (((weekSofia * 3)/4) + h + ((p * 2)/3));
+
+        switch (year) {
+            case "leap":
+                games = Math.floor(games + (games * 0.15));
+                break;
+            case "normal":
+                games = Math.floor(games);
+                break;
+            default:
+                games = Math.floor(games);
+                break;
+        }
+        System.out.printf("%.0f", games);
+    }
+}
